a136854a33b8fdd42b57ff695f96d642084fffbe
kjm9547 <woals8115@naver.com>
2023-09-10 16:26:38 +0900

    befor safe area part

---
+++ component/src/component/Divider.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View } from "react-native";
+
+export class Divider extends React.Component{
+    render(){
+        return(
+            <View style={{alignSelf:"stretch", marginHorizontal:24,borderWidth:0.5, borderColor:'gray'}}>
+
+            </View>
+        )
+    }
+}
